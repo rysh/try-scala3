4dff6c994783993cab1b67021618a4d781e60198
r.ishibashi <rysh.cact@gmail.com>
2019-11-01 23:49:01 +0900

    nested case

--- src/main/scala/AutomaticTupling.scala
+++ src/main/scala/AutomaticTupling.scala
@@ -14,10 +14,16 @@ object AutomaticTupling {
       case Hoge(x, y) => x + y
     }
     ys.map {
-      (x, y) => x + y
+      (x, y) ⇒ x + y
     }
     ys.map(_ + _)
+
+    List(Fuga(1,Piyo(2,3))).map {
+      case Fuga(x, Piyo(y, z)) ⇒ x + y + z
+    }
     "start new AutomaticTupling"
   }
 }
 case class Hoge(x:Int,y:Int)
+case class Fuga(x:Int,y:Piyo)
+case class Piyo(x:Int,y:Int)
